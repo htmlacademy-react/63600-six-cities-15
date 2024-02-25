114607eadd3375b656985a15290a9f82122f2f4b
Marianna Zhukova <marsoupilamie@gmail.com>
2024-02-25 21:17:23 +0300

    В props передано количество предложений аренды

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app.tsx';
 
+const placesCount = 325;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App placesCount={placesCount}/>
   </React.StrictMode>
 );
